fd466757322d47b1569abcac33834a940077c9f7
Fede Lopez <fede.lopez@gmail.com>
2015-12-09 20:38:22 +1100

    Fixes #2 Fixes #11

--- src/main/scala-2.11/cat/pseudocodi/GameLoopActor.scala
+++ src/main/scala-2.11/cat/pseudocodi/GameLoopActor.scala
@@ -1,15 +1,22 @@
 package cat.pseudocodi
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, Cancellable, Props}
+import cat.pseudocodi.GameLoopActor._
 
 import scala.concurrent.duration._
 
 /**
- * @author Fede
- */
+  * @author Fede
+  */
 object GameLoopActor {
 
+  case object TitleScreenRequested
+
   case object GameStarted
+
+  case object GameFinished
+
+  case object ExitRequested
 
 }
 
@@ -17,13 +24,26 @@ class GameLoopActor extends Actor {
 
   import context.dispatcher
 
+  var schedule: Option[Cancellable] = None
   val scene = context.actorOf(Props[SceneActor])
 
-  scene ! SceneActor.ShowScene
+  scene ! SceneActor.DrawTitleScene
 
   override def receive: Receive = {
-    case GameLoopActor.GameStarted => context.system.scheduler.schedule(Duration.Zero, 20.millis, scene, SceneActor.RedrawScene)
+    case GameLoopActor.GameStarted =>
+      cancelSchedule()
+      schedule = Some(context.system.scheduler.schedule(Duration.Zero, 20.millis, scene, SceneActor.RedrawScene))
+    case GameLoopActor.GameFinished =>
+      cancelSchedule()
+      schedule = Some(context.system.scheduler.schedule(Duration.Zero, 20.millis, scene, SceneActor.RedrawGameOverScene))
+    case TitleScreenRequested =>
+      cancelSchedule()
+      scene ! SceneActor.DrawTitleScene
+    case ExitRequested =>
+      cancelSchedule()
+      context.system.terminate()
   }
 
+  def cancelSchedule() = schedule.foreach((cancellable: Cancellable) => cancellable.cancel())
 }
 
--- src/main/scala-2.11/cat/pseudocodi/SceneActor.scala
+++ src/main/scala-2.11/cat/pseudocodi/SceneActor.scala
@@ -6,7 +6,7 @@ import java.awt.{Color, Font, Frame, _}
 import java.io.File
 
 import akka.actor.{Actor, ActorRef, Props}
-import cat.pseudocodi.GameLoopActor.GameStarted
+import cat.pseudocodi.GameLoopActor._
 import cat.pseudocodi.SceneActor._
 
 import scala.swing.{Graphics2D, Rectangle}
@@ -17,36 +17,42 @@ import scala.util.Random
   */
 object SceneActor {
 
-  case object ShowScene
+  case object InitScene
+
+  case object DrawTitleScene
 
   case object RedrawScene
+
+  case object RedrawGameOverScene
 
 }
 
 class SceneActor extends Actor {
 
+  var frame: Frame = null
   var bounds: Rectangle = null
   var paddle1, paddle2: Paddle = null
   var ball: Ball = null
-
   var playing = false
+  var paddle1PressedKey: Option[Int] = None
+  var paddle2PressedKey: Option[Int] = None
 
-  var frame: Frame = null
   val smallFont = Font.createFont(Font.TRUETYPE_FONT, new File(getClass.getResource("/arcade_classic.ttf").getFile)).deriveFont(36f)
   val mediumFont = smallFont.deriveFont(72f)
   val largeFont = smallFont.deriveFont(144f)
 
-  var paddle1PressedKey: Option[Int] = None
-  var paddle2PressedKey: Option[Int] = None
-
+  var senderActor: ActorRef = null
   val soundActor = context.actorOf(Props[SoundActor])
 
+  override def preStart() = initScene()
+
   override def receive: Receive = {
-    case ShowScene => initScene(sender()); drawTitleScreen()
+    case DrawTitleScene => drawTitleScreen()
     case RedrawScene => drawGameScene()
+    case RedrawGameOverScene => drawGameOverScene()
   }
 
-  def initScene(sender: ActorRef) = {
+  def initScene() = {
     val env: GraphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment
     val device: GraphicsDevice = env.getDefaultScreenDevice
     val gc: GraphicsConfiguration = device.getDefaultConfiguration
@@ -55,10 +61,18 @@ class SceneActor extends Actor {
     frame.setIgnoreRepaint(true)
     frame.addKeyListener(new KeyAdapter {
       override def keyPressed(e: KeyEvent) = e.getKeyCode match {
-        case VK_SPACE if !playing => playing = true; sender ! GameStarted
+        case VK_SPACE if !playing => playing = true; senderActor ! GameStarted
         case VK_W | VK_S if playing => paddle1PressedKey = Some(e.getKeyCode)
         case VK_O | VK_K if playing => paddle2PressedKey = Some(e.getKeyCode)
-        case _ => ()
+        case VK_ESCAPE =>
+          if (playing) {
+            playing = false
+            senderActor ! TitleScreenRequested
+          } else {
+            frame.dispose()
+            senderActor ! ExitRequested
+          }
+        case _ => println("Wrong key")
       }
 
       override def keyReleased(e: KeyEvent) = e.getKeyCode match {
@@ -70,14 +84,17 @@ class SceneActor extends Actor {
     device.setFullScreenWindow(frame)
     bounds = frame.getBounds
     frame.createBufferStrategy(2)
-    paddle1 = new Paddle("p1", bounds.width / 2 - (bounds.width / 3), bounds.height / 2 - Paddle.height / 2, bounds)
-    paddle2 = new Paddle("p2", bounds.width / 2 + (bounds.width / 3), bounds.height / 2 - Paddle.height / 2, bounds)
-    ball = initBall(randomSign())
   }
 
   def drawTitleScreen() = {
-    val titleMetrics = fontMetrics("PONG", mediumFont)
-    val subtitleMetrics = fontMetrics("PRESS SPACE", smallFont)
+    playing = false
+    senderActor = sender()
+    paddle1 = new Paddle("p1", bounds.width / 2 - (bounds.width / 3), bounds.height / 2 - Paddle.height / 2, bounds)
+    paddle2 = new Paddle("p2", bounds.width / 2 + (bounds.width / 3), bounds.height / 2 - Paddle.height / 2, bounds)
+    ball = initBall(randomSign())
+
+    val title = fontMetrics("PONG", mediumFont)
+    val subtitle = fontMetrics("PRESS SPACE", smallFont)
 
     val bufferStrategy = frame.getBufferStrategy
     val g = bufferStrategy.getDrawGraphics
@@ -86,9 +103,9 @@ class SceneActor extends Actor {
       g.fillRect(0, 0, bounds.width, bounds.height)
       g.setColor(Color.white)
       g.setFont(mediumFont)
-      g.drawString("PONG", bounds.width / 2 - (titleMetrics._1 / 2), bounds.height / 2 - titleMetrics._2 / 2)
+      g.drawString("PONG", bounds.width / 2 - (title._1 / 2), bounds.height / 2 - title._2 / 2)
       g.setFont(smallFont)
-      g.drawString("PRESS SPACE", bounds.width / 2 - (subtitleMetrics._1 / 2), bounds.height / 2 + subtitleMetrics._2 / 2)
+      g.drawString("PRESS SPACE", bounds.width / 2 - (subtitle._1 / 2), bounds.height / 2 + subtitle._2 / 2)
 
       bufferStrategy.show()
       g.dispose()
@@ -123,6 +140,9 @@ class SceneActor extends Actor {
       paddle1 = paddle1.scoreUp()
     }
     ball = ball.move()
+    if (paddle1.score == 11 || paddle2.score == 11) {
+      sender() ! GameFinished
+    }
 
     //REPAINT
     val bufferStrategy = frame.getBufferStrategy
@@ -146,6 +166,38 @@ class SceneActor extends Actor {
       bufferStrategy.show()
       g.dispose()
     }
+  }
+
+  def drawGameOverScene() = {
+    //COLLISIONS: BALL
+    if (ball.y < 1 || ball.y + ball.h >= bounds.height) {
+      ball = new Ball(ball.x, ball.y, ball.dx, ball.dy * -1)
+    } else if (ball.x < 1 || ball.x + ball.w > bounds.width) {
+      ball = new Ball(ball.x, ball.y, ball.dx * -1, ball.dy)
+    }
+    ball = ball.move()
+
+    //REPAINT
+    val bufferStrategy = frame.getBufferStrategy
+    val g = bufferStrategy.getDrawGraphics.asInstanceOf[Graphics2D]
+    if (!bufferStrategy.contentsLost) {
+      g.setColor(Color.black)
+      g.fillRect(0, 0, bounds.width, bounds.height)
+      g.setColor(Color.white)
+      g.fillRect(ball.x, ball.y, ball.w, ball.h)
+      g.setStroke(new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1.0f, Array(12f), 0.0f))
+      g.drawLine(bounds.width / 2, 0, bounds.width / 2, bounds.height)
+
+      g.setFont(largeFont)
+      val paddle1Metrics: (Width, Height) = fontMetrics(paddle1.score.toString, largeFont)
+      g.drawString(paddle1.score.toString, bounds.width / 4 - (paddle1Metrics._1 / 2), bounds.height / 4 - paddle1Metrics._2 / 2)
+      val paddle2Metrics: (Width, Height) = fontMetrics(paddle2.score.toString, largeFont)
+      g.drawString(paddle2.score.toString, bounds.width - bounds.width / 4 - (paddle2Metrics._1 / 2), bounds.height / 4 - paddle2Metrics._2 / 2)
+
+      bufferStrategy.show()
+      g.dispose()
+    }
+
   }
 
   def initBall(dx: Int) = new Ball(bounds.width / 2 - Ball.width / 2, bounds.height / 2 - Ball.height / 2, dx, Random.nextInt(Ball.width * 2) * randomSign())
